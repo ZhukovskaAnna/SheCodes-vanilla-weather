5c5c8323385e4721ba5a97f143f562bfc6a31f2e
Anna Zhukovska <zhukovska.anna@gmail.com>
2024-01-15 16:35:22 +0000

    inject the forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -61,3 +61,23 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Lisbon");
+
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      ` <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">🌤</div>
+            <div class="weather-forecast-temperatures">
+              <span class="weather-forecast-temperature-max"> <strong>18°</strong></span>
+              <span class="weather-forecast-temperature-min">12°</span>
+            </div>
+          </div>`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+displayForecast();
